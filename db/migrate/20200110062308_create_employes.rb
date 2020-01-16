43774d1c743a29ae2308f0fe61c80866a59b92e6
Mamathap37 <mamatha@bytesqueue.com>
2020-01-16 12:14:12 +0530

    first commit

---
+++ db/migrate/20200110062308_create_employes.rb
@@ -0,0 +1,10 @@
+class CreateEmployes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :employes do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
